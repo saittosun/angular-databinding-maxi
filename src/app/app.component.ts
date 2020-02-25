058645f527bbfb89945ea613ac251ed0cf64b7b6
Tosun Sait <tosunsa@cronos.be>
2020-02-25 09:56:58 +0100

    binding to custom properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    {type: 'server', name: 'test server', content: 'just a test'}
+  ];
 
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+  // tslint:disable-next-line:max-line-length
+  // this is TypeScript syntax for defining the type to make sure that element may only have this type and we then know that an element will have what we're trying to access
+  // tslint:disable-next-line:max-line-length
+  // this is TypeScript syntax for defining the type to make sure that element may only have this type and we then know that an element will have what we're trying to access
+  @Input() element: {type: string, name: string, content: string};
   constructor() { }
 
   ngOnInit(): void {
